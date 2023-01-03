91bbd0d4024e484783c0d89585f18049802e7183
Sam-Dunlap <sam.dunlap15@gmail.com>
2023-01-03 15:06:52 -0800

    quick file organization

--- Scripts/StartMenuVBox.gd
+++ Scripts/StartMenuVBox.gd
@@ -3,6 +3,6 @@ extends VBoxContainer
 
 func add_gamer_tag_inputs(numPlayers):
 	for i in range(numPlayers):
-		var tag = load("TagInput.tscn").instance()
+		var tag = load("res://TagInput.tscn").instance()
 		tag.get_child(0).text = "Player " + str(i + 1) + ":"
 		add_child(tag)
